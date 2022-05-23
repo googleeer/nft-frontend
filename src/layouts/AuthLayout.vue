ec7bf3447cbbf5690dc1f11a7a9ce48f23dfed0e
Derek <derektesting11@gmail.com>
2022-05-24 00:59:05 +0300

    add desktop ver of AuthLayout

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -14,41 +14,76 @@ export default defineComponent({
 
 <template>
   <div class="flex direction-column container align-center flex-grow-1">
-    <img class="logo" src="../assets/images/logo.png" alt="WEEDAR" />
+    <img class="logo" src="../assets/images/logo.svg" alt="WEEDAR" />
     <div class="container__form flex direction-column flex-grow-1">
-      <h1 class="title">{{ title }}</h1>
-      <p v-if="text" class="text">{{ text }}</p>
+      <h1 class="container__form--title">{{ title }}</h1>
+      <p v-if="text" class="container__form--text">{{ text }}</p>
       <slot name="text"></slot>
-      <slot name="form"></slot>
+      <slot name="form" class="container__form--btn"></slot>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+::v-slotted(form) {
+  justify-content: flex-start;
+}
 .container {
-  padding-top: 24px;
+  background-image: url("~@/assets/images/bg-logo.svg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  padding-top: 133px;
+  text-align: center;
   padding-left: 15px;
   padding-right: 15px;
-  text-align: left;
-
+  background-position: center center;
+  @media screen and (max-width: 768px) {
+    padding-top: 24px;
+    text-align: left;
+  }
   &__form {
-    max-width: 335px;
+    max-width: 312px;
+    &--title {
+      font-size: 48px;
+      font-weight: 600;
+      padding-bottom: 38px;
+      line-height: 130%;
+      @media screen and (max-width: 768px) {
+        font-size: 32px;
+        line-height: 130%;
+        margin-bottom: 15px;
+      }
+    }
+    &--text {
+      line-height: 150%;
+      font-size: 20px;
+      padding-bottom: 38px;
+      @media screen and (max-width: 768px) {
+        font-size: 20px;
+        line-height: 150%;
+        margin-bottom: 30px;
+      }
+    }
+  }
+  &--btn {
+    max-width: 20px;
+  }
+  ::v-deep(.button) {
+    margin-top: 116px;
   }
 }
 .logo {
-  width: 70px;
-  margin-bottom: 60px;
+  width: 104px;
+  margin-bottom: 77px;
+  @media screen and (max-width: 768px) {
+    width: 70px;
+    margin-bottom: 60px;
+  }
 }
 
-.title {
-  font-size: 32px;
-  line-height: 130%;
-  margin-bottom: 15px;
-}
-
-.text {
-  font-size: 20px;
-  line-height: 150%;
-  margin-bottom: 30px;
-}
+//.text {
+//  font-size: 20px;
+//  line-height: 150%;
+//  margin-bottom: 30px;
+//}
 </style>
