4356d2187206a7a3c393b800a97779160368d5a7
Derek <derektesting11@gmail.com>
2022-05-24 11:37:42 +0300

    fix auth background and position of elements center

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -13,34 +13,39 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="flex direction-column container align-center flex-grow-1">
-    <img class="logo" src="../assets/images/logo.svg" alt="WEEDAR" />
-    <div class="container__form flex direction-column flex-grow-1">
-      <h1 class="container__form--title">{{ title }}</h1>
-      <p v-if="text" class="container__form--text">{{ text }}</p>
-      <slot name="text"></slot>
-      <slot name="form" class="container__form--btn"></slot>
+  <div class="wrapper flex justify-center align-center">
+    <div class="flex direction-column container align-center">
+      <img class="logo" src="../assets/images/logo.svg" alt="WEEDAR" />
+      <div class="container__form flex direction-column flex-grow-1">
+        <h1 class="container__form--title">{{ title }}</h1>
+        <p v-if="text" class="container__form--text">{{ text }}</p>
+        <slot name="text"></slot>
+        <slot name="form" class="container__form--btn"></slot>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.wrapper {
+  height: 100vh;
+}
 ::v-slotted(form) {
   justify-content: flex-start;
+  @media screen and (max-width: 768px) {
+    justify-content: space-between;
+  }
 }
 .container {
-  background-image: url("~@/assets/images/bg-logo.svg");
-  background-size: cover;
-  background-repeat: no-repeat;
+  position: relative;
+  width: 100%;
+  //background-image: url("~@/assets/images/bg-logo.svg");
+  //background-repeat: no-repeat;
   padding-top: 133px;
   text-align: center;
   padding-left: 15px;
   padding-right: 15px;
   background-position: center center;
-  @media screen and (max-width: 768px) {
-    padding-top: 24px;
-    text-align: left;
-  }
   &__form {
     max-width: 312px;
     &--title {
@@ -80,7 +85,18 @@ export default defineComponent({
     margin-bottom: 60px;
   }
 }
-
+.wrapper::before {
+  content: "";
+  position: absolute;
+  background-image: url("~@/assets/images/bg-logo.svg");
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  @media screen and (max-width: 768px) {
+    content: none;
+  }
+}
 //.text {
 //  font-size: 20px;
 //  line-height: 150%;
