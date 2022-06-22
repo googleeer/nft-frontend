6085f020b67c93db587a63722bb368ac1606b61b
Derek <derektesting11@gmail.com>
2022-06-22 12:09:14 +0300

    fix width menu btn

--- src/components/baseLayout/BaseBurger.vue
+++ src/components/baseLayout/BaseBurger.vue
@@ -24,7 +24,7 @@ export default defineComponent({
         <svg
           v-if="!isOpen"
           width="34"
-          height="26"
+          height="35"
           viewBox="0 0 34 26"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
